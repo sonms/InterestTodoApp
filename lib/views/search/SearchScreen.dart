6b68142a4a5db542b99d683211b8fe1f893fa87c
sonms <smsms5676@naver.com>
2025-03-14 17:51:30 +0900

    feat/검색을 위한 TextField 세팅

--- lib/views/search/SearchScreen.dart
+++ lib/views/search/SearchScreen.dart
@@ -1,14 +1,32 @@
 import 'package:flutter/material.dart';
 
-class SearchScreen extends StatelessWidget {
+class SearchScreen extends StatefulWidget {
   const SearchScreen({super.key});
+
+  @override
+  State<StatefulWidget> createState() {
+    return _SearchScreen();
+  }
+}
+
+class _SearchScreen extends State<SearchScreen> {
+
+  final myController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text ("search"),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(
+              margin: EdgeInsets.all(8),
+              child:TextField(
+                controller: myController,
+              )
+          )
+        ],
+      ),
     );
-    throw UnimplementedError();
   }
-
 }
